062c5e9052e3eaf950c4303ac09d72678b804e1c
Carlos <karucupcake@gmail.com>
2022-10-03 17:08:47 +0200

    terminado

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var num1=7,num2=5;
+if (num1!=num2) {
+    if (num1>num2) {
+        console.log(num1+" es el mayor");
+    }else {
+        console.log(num2+" es el mayor");
+    }
+    
+}else{
+    console.log("Son iguales");
+}
